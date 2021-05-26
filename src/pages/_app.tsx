fd1a7cb65d31c1c0fe45dbc37613c8493b73212a
Nick Burka <nick@silverorange.com>
2021-05-26 09:28:39 -0400

    move into a src dir

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from 'next/app';
-import '../styles/global.css';
+import '../../styles/global.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
