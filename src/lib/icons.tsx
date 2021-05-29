f73ca64490df767b5d9014f886509f32ce20d18d
Nick Burka <nick@silverorange.com>
2021-05-29 11:08:04 -0400

    basic icons structure

---
+++ src/lib/icons.tsx
@@ -0,0 +1,40 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+
+const ICONS_DIRECTORY = path.join(process.cwd(), 'public', 'icons');
+
+export interface Category {
+  title: string;
+  icons: Icon[];
+}
+
+export interface Icon {
+  title: string;
+  path: string;
+}
+
+export async function getCategoriesAndIcons(): Promise<Category[]> {
+  const dirNames = await fs.readdir(ICONS_DIRECTORY);
+  return await Promise.all(
+    dirNames.map(async (dirName) => {
+      return {
+        title: dirName,
+        icons: await getIcons(dirName)
+      };
+    })
+  );
+}
+
+async function getIcons(dirName: string): Promise<Icon[]> {
+  const dirPath = path.join(ICONS_DIRECTORY, dirName);
+  const fileNames = await fs.readdir(dirPath);
+  return await Promise.all(
+    fileNames.map(async (fileName) => {
+      const fullPath = path.join(ICONS_DIRECTORY, dirName, fileName);
+      return {
+        title: fileName.replace(/\.[^/.]+$/, ''),
+        path: fullPath
+      };
+    })
+  );
+}
