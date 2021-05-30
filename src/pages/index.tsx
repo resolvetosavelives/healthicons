af94b3a12f6c65ce5c450be6c8a8e52989739d4e
Daniel Burka <danielburka@gmail.com>
2021-05-30 14:14:57 +0100

    Formatted the search, added some icons. Improved some other formatting.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,7 +36,7 @@ export default function Home({ categories }: HomeProps) {
     <div className="container">
       <Head>
         <title>Health Icons</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
 
       <TopBar />
@@ -53,7 +53,7 @@ export default function Home({ categories }: HomeProps) {
           <input
             type="text"
             className={styles.filterBoxInput}
-            placeholder="Find Icons"
+            placeholder="Search for icons..."
             onChange={(e) => setQuery(e.target.value)}
           />
         </label>
