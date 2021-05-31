88ddcdf329ebfe32e6de43198fc281a0b0f4243f
Daniel Burka <danielburka@gmail.com>
2021-05-31 23:00:01 +0100

    Fixed a broken icon. Added BETA to logo. Added @2x PNG downloads for all icons.

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -6,7 +6,9 @@ export function TopBar() {
     <nav className={styles.topBar}>
       <div className={styles.topbarTitle}>
         <Link href="/">
-          <a className={styles.title}>Health icons</a>
+          <a className={styles.title}>
+            Health icons <sup>BETA</sup>
+          </a>
         </Link>
       </div>
       <ul className={styles.topbarLinks}>
