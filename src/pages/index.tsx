1ec36e73215f30a5cf9a49477c00ca49e36e4f2c
Nick Burka <nick@silverorange.com>
2021-06-01 17:10:28 -0400

    show a total count of icons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -39,6 +39,10 @@ export default function Home({ categories }: HomeProps) {
     return filteredIcons;
   }, [query, categories]);
 
+  const totalIconCount = categories.flatMap((category) => {
+    return category.icons;
+  }).length;
+
   return (
     <div className="container">
       <Head>
@@ -84,7 +88,7 @@ export default function Home({ categories }: HomeProps) {
           <input
             type="text"
             className={styles.filterBoxInput}
-            placeholder="Search for icons..."
+            placeholder={`Search ${totalIconCount} Icons...`}
             onChange={(e) => setQuery(e.target.value)}
           />
         </label>
