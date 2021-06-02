fee83606079b57da4b65f1fb7805c91d4353c1e5
Daniel Burka <danielburka@gmail.com>
2021-06-02 21:30:50 +0100

    Refactored bullets on the homepage. Added a class for H6. Added a top margin to H1 on about.

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -174,34 +174,40 @@ export default function About() {
             </p>
 
             <h2 id="contribute">How to contribute</h2>
-            <p className={styles.aboutContact}>
-              <strong>Get in touch</strong>
-              <br />
-              Join our open Slack channel by emailing{' '}
-              <a href="mailto:contact@healthicons.org">
-                contact@healthicons.org
-              </a>
-            </p>
-            <p className={styles.aboutDraw}>
-              <strong>Contribute icons</strong>
-              <br />
-              Browse the{' '}
-              <a href="https://www.figma.com/file/mbsBVYXECIOl5E0kkGAiC2/?node-id=978%3A3">
-                Figma
-              </a>{' '}
-              and contribute icons in the "New icons" section. Please read the
-              code of conduct (below) and the styleguide (in the Figma)
-              carefully before contributing.
-            </p>
-            <p className={styles.aboutDev}>
-              <strong>Improve the website</strong>
-              <br />
-              Developers can browse the{' '}
-              <a href="https://github.com/resolvetosavelives/healthicons/issues">
-                issues on Github
-              </a>{' '}
-              and pick up projects to improve this website.
-            </p>
+            <div className={styles.aboutBullet}>
+              <h6>Get in touch</h6>
+              <p>
+                Join our open Slack channel by emailing{' '}
+                <a href="mailto:contact@healthicons.org">
+                  contact@healthicons.org
+                </a>
+              </p>
+              <img src="/ui/slack.svg" width="32" height="32" alt="" />
+            </div>
+            <div className={styles.aboutBullet}>
+              <h6>Contribute icons</h6>
+              <p>
+                Browse the{' '}
+                <a href="https://www.figma.com/file/mbsBVYXECIOl5E0kkGAiC2/?node-id=978%3A3">
+                  Figma
+                </a>{' '}
+                and contribute icons in the "New icons" section. Please read the
+                code of conduct (below) and the styleguide (in the Figma)
+                carefully before contributing.
+              </p>
+              <img src="/ui/figma.svg" width="32" height="32" alt="" />
+            </div>
+            <div className={styles.aboutBullet}>
+              <h6>Improve the website</h6>
+              <p>
+                Developers can browse the{' '}
+                <a href="https://github.com/resolvetosavelives/healthicons/issues">
+                  issues on Github
+                </a>{' '}
+                and pick up projects to improve this website.
+              </p>
+              <img src="/ui/code.svg" width="32" height="32" alt="" />
+            </div>
 
             <h2 id="license">License</h2>
             <div className={styles.codeBlock}>
