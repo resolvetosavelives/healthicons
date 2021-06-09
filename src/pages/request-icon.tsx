7c370254ec7df2082ec2bc4596edce71da1d6cd9
Daniel Burka <danielburka@gmail.com>
2021-06-09 11:29:51 +0100

    Moved the H1 H2 classes to Global. Formatted the Request Icon page to look more like About.

--- src/pages/request-icon.tsx
+++ src/pages/request-icon.tsx
@@ -38,9 +38,7 @@ export default function RequestIcon() {
       <main id="main">
         <div className={styles.contentContainer}>
           <div className={styles.contentMain}>
-            <h1 id="request" className="title">
-              Request an Icon
-            </h1>
+            <h1 id="request">Request an icon</h1>
             <p className={styles.contentIntro}>
               Need an icon for your project?{' '}
               <a href="https://github.com/resolvetosavelives/healthicons/issues/new?assignees=&amp;labels=icon+request&amp;template=icon-request.md&amp;title=Icon+Request%3A+%5Bname+of+icon%5D">
@@ -49,12 +47,19 @@ export default function RequestIcon() {
               and we will do our best to respond.
             </p>
             <p>
-              <strong>Important</strong>: Please provide as much detail as you
-              can to help us design the icon. Including examples of similar
-              icons, photos, diagrams, or as much detail and description as
-              possible helps us create the best icon possible.
+              This is an all-volunteer effort, so please be patient. If you need
+              general user interface icons, we really like the{' '}
+              <a href="https://feathericons.com/">Feather</a> and{' '}
+              <a href="https://remixicon.com/">Remix</a> open source icon sets,
+              which match Health Icons pretty well.
             </p>
-            <h3>Example Icon Request</h3>
+            <h2>Example icon request</h2>
+            <p>
+              Important: Please provide as much detail as you can to help us
+              design the icon. Including examples of similar icons, photos,
+              diagrams, or as much detail and description as possible helps us
+              create the best icon possible.
+            </p>
             <div className={styles.example}>
               <h4 className={styles.exampleTitle}>Icon Request: Stent</h4>
               <p>
@@ -69,7 +74,7 @@ export default function RequestIcon() {
                 it's not just a tube?
               </p>
               <p>Some examples I've seen:</p>
-              <p>
+              <p class="mt-2">
                 <img
                   src="/request-icon/stent-example1.png"
                   alt="Example Stent 1"
