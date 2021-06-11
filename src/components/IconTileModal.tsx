75d32549884d6db0299728d14faf585e624ae0d2
Daniel Burka <danielburka@gmail.com>
2021-06-11 16:51:34 +0100

    Improved the scrolling of the icon modal

--- src/components/IconTileModal.tsx
+++ src/components/IconTileModal.tsx
@@ -19,72 +19,74 @@ export function IconTileModal(props: IconTileModalProps) {
       overlayClassName={styles.modalBody}
       closeTimeoutMS={200}
     >
-      <>
-        <button onClick={props.onClose} className={styles.modalClose}>
-          ×
-        </button>
-        <img
-          src={`icons/svg/${props.iconType}/${props.icon.path}/${props.icon.fileName}.svg`}
-          className={styles.modalImage}
-          width="96"
-          height="96"
-          alt=""
-        />
-        <div className={styles.modalLabel}>Icon</div>
-        <div className={styles.modalTitle}>{props.icon.title}</div>
-
-        <div className={styles.modalButtons}>
-          <a
-            href={`icons/svg/${props.iconType}/${props.icon.path}/${props.icon.fileName}.svg`}
-            download={`${props.icon.fileName}.svg`}
-            className={styles.modalButton}
-          >
-            <span>SVG</span>
-          </a>
-          <a
-            href={`icons/png/${props.iconType}/${props.icon.path}/${props.icon.fileName}.png`}
-            download={`${props.icon.fileName}.png`}
-            className={styles.modalButton}
-          >
-            <span>48px PNG</span>
-          </a>
-          <a
-            href={`icons/png/${props.iconType}/${props.icon.path}/${props.icon.fileName}@2x.png`}
-            download={`${props.icon.fileName}@2x.png`}
-            className={styles.modalButton}
-          >
-            <span>96px PNG</span>
-          </a>
-        </div>
-        {props.icon.tags.length > 0 && (
-          <>
-            <div className={styles.modalLabel}>Tags</div>
-            <div className={styles.modalTags}>
-              {props.icon.tags.map((tag) => (
-                <span key={tag}>{tag}</span>
-              ))}
-            </div>
-          </>
-        )}
-      </>
-      <div className={styles.modalLabel}>License</div>
-      <p className={styles.modalLicense}>
-        <a
-          rel="license"
-          href="http://creativecommons.org/publicdomain/zero/1.0/"
-        >
+      <div className={styles.modalWrapper}>
+        <>
           <img
-            src="http://i.creativecommons.org/p/zero/1.0/88x31.png"
-            alt="CC0"
+            src={`icons/svg/${props.iconType}/${props.icon.path}/${props.icon.fileName}.svg`}
+            className={styles.modalImage}
+            width="96"
+            height="96"
+            alt=""
           />
-        </a>
-        To the extent possible under law,
-        <a rel="dct:publisher" href="https://healthicons.org">
-          <span property="dct:title"> Health Icons </span>
-        </a>
-        has waived all copyright and related or neighboring rights to available
-        icons.
-      </p>
+          <div className={styles.modalLabel}>Icon</div>
+          <div className={styles.modalTitle}>{props.icon.title}</div>
+
+          <div className={styles.modalButtons}>
+            <a
+              href={`icons/svg/${props.iconType}/${props.icon.path}/${props.icon.fileName}.svg`}
+              download={`${props.icon.fileName}.svg`}
+              className={styles.modalButton}
+            >
+              <span>SVG</span>
+            </a>
+            <a
+              href={`icons/png/${props.iconType}/${props.icon.path}/${props.icon.fileName}.png`}
+              download={`${props.icon.fileName}.png`}
+              className={styles.modalButton}
+            >
+              <span>48px PNG</span>
+            </a>
+            <a
+              href={`icons/png/${props.iconType}/${props.icon.path}/${props.icon.fileName}@2x.png`}
+              download={`${props.icon.fileName}@2x.png`}
+              className={styles.modalButton}
+            >
+              <span>96px PNG</span>
+            </a>
+          </div>
+          {props.icon.tags.length > 0 && (
+            <>
+              <div className={styles.modalLabel}>Tags</div>
+              <div className={styles.modalTags}>
+                {props.icon.tags.map((tag) => (
+                  <span key={tag}>{tag}</span>
+                ))}
+              </div>
+            </>
+          )}
+        </>
+        <div className={styles.modalLabel}>License</div>
+        <p className={styles.modalLicense}>
+          <a
+            rel="license"
+            href="http://creativecommons.org/publicdomain/zero/1.0/"
+          >
+            <img
+              src="http://i.creativecommons.org/p/zero/1.0/88x31.png"
+              alt="CC0"
+            />
+          </a>
+          To the extent possible under law,
+          <a rel="dct:publisher" href="https://healthicons.org">
+            <span property="dct:title"> Health Icons </span>
+          </a>
+          has waived all copyright and related or neighboring rights to
+          available icons.
+        </p>
+      </div>
+      <button onClick={props.onClose} className={styles.modalClose}>
+        ×
+      </button>
     </ReactModal>
   );
 }
