37d07d6a906e780c122cb903a29f9b2f011a6b4b
Nick Burka <nick@silverorange.com>
2023-06-01 15:47:58 -0400

    much better sed script

--- src/scripts/removeSvgBackgrounds.sh
+++ src/scripts/removeSvgBackgrounds.sh
@@ -3,6 +3,9 @@
 DIRECTORY=$(cd $(dirname $0) && pwd)
 SVGS=$DIRECTORY/../../public/icons/svg
 
-find $SVGS -type f -name "*.svg" -exec sed -i -e 's/fill="none"/fill="currentColor"/g' {} +
-find $SVGS -type f -name "*.svg" -exec sed -i -e 's/fill="white"/fill="none"/g' {} +
-find $SVGS -type f -name "*.svg" -exec sed -i -e 's/fill="#333333"//g' {} +
+find $SVGS -type f -name "*.svg" \
+    -exec sed -i "" \
+    -e 's/fill="white"/fill="none"/g' \
+    -e 's/fill="#333333"/fill="currentColor"/g' \
+    -e 's/fill="black"/fill="currentColor"/g' \
+    {} +
