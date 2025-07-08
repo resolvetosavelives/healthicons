af5c685de2328edabe653fbbc05dbff4c714c5a7
Ritik Jaiswal <ritik12103032000@gmail.com>
2025-07-09 01:46:19 +0530

    [issue #55] Feature: Include SVG markup in the modal and make it easy to copy to clipboard

--- src/components/IconTileModal.tsx
+++ src/components/IconTileModal.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './IconTileModal.module.scss';
 import ReactModal from 'react-modal';
 import { useRouter } from 'next/navigation';
@@ -16,6 +16,7 @@ interface IconTileModalProps {
 }
 
 export function IconTileModal(props: IconTileModalProps) {
+  const [iconCode, setIconCode] = useState(null);
   const router = useRouter();
   const dispatch = useDispatch();
 
@@ -31,6 +32,49 @@ export function IconTileModal(props: IconTileModalProps) {
     dispatch(setKeywords(tag));
     props.onClose();
   };
+
+  const handleCopyToClipboard = () => {
+    if (iconCode) {
+      if (navigator.clipboard && window.isSecureContext) {
+        navigator.clipboard.writeText(iconCode).catch(() => {
+          const textarea = document.createElement('textarea');
+          textarea.value = iconCode;
+          textarea.style.position = 'fixed';
+          textarea.style.left = '-9999px';
+          document.body.appendChild(textarea);
+          textarea.focus();
+          textarea.select();
+          try {
+            document.execCommand('copy');
+          } finally {
+            document.body.removeChild(textarea);
+          }
+        });
+      } else {
+        const textarea = document.createElement('textarea');
+        textarea.value = iconCode;
+        textarea.style.position = 'fixed';
+        textarea.style.left = '-9999px';
+        document.body.appendChild(textarea);
+        textarea.focus();
+        textarea.select();
+        try {
+          document.execCommand('copy');
+        } finally {
+          document.body.removeChild(textarea);
+        }
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetch(
+      `/icons/svg/${props.iconType}/${props.icon.category}/${props.icon.id}.svg`
+    )
+      .then((res) => res.text())
+      .then((text) => setIconCode(text));
+    return () => setIconCode(null);
+  }, [props.icon.category, props.icon.id, props.iconType]);
 
   useEffect(() => {
     const keyDownHandler = (e) => {
@@ -106,65 +150,85 @@ export function IconTileModal(props: IconTileModalProps) {
           />
           <div className={styles.modalTitle}>{props.icon.title}</div>
 
-          <div className={styles.modalLabel}>Standard version (48px grid)</div>
-          <div className={styles.modalButtons}>
-            <a
-              href={`/icons/svg/${props.iconType}/${props.icon.category}/${props.icon.id}.svg`}
-              download={`${props.icon.id}.svg`}
-              className={styles.modalButton}
-            >
-              <span>SVG</span>
-            </a>
-            <a
-              href={`/icons/png/${props.iconType}/${props.icon.category}/${props.icon.id}.png`}
-              download={`${props.icon.id}.png`}
-              className={styles.modalButton}
-            >
-              <span>48px PNG</span>
-            </a>
-            <a
-              href={`/icons/png/${props.iconType}/${props.icon.category}/${props.icon.id}@2x.png`}
-              download={`${props.icon.id}@2x.png`}
-              className={styles.modalButton}
-            >
-              <span>96px PNG</span>
-            </a>
-          </div>
-          {props.icon.formats.includes('24px') && (
-            <>
-              <img
-                src={`/icons/svg/${props.iconType}-24px/${props.icon.category}/${props.icon.id}.svg`}
-                className={styles.modal24pxImage}
-                width="24"
-                height="24"
-                alt=""
-              />
-              <div className={styles.modalLabel}>Small version (24px grid)</div>
-              <div className={styles.modal24pxVersions}>
+          <div className={styles.userActionsContainer}>
+            <div className={styles.iconDownloadContainer}>
+              <div className={styles.modalLabel}>
+                Standard version (48px grid)
+              </div>
+              <div className={styles.modalButtons}>
                 <a
-                  href={`/icons/svg/${props.iconType}-24px/${props.icon.category}/${props.icon.id}.svg`}
-                  download={`${props.icon.id}-24px.svg`}
+                  href={`/icons/svg/${props.iconType}/${props.icon.category}/${props.icon.id}.svg`}
+                  download={`${props.icon.id}.svg`}
                   className={styles.modalButton}
                 >
                   <span>SVG</span>
                 </a>
                 <a
-                  href={`/icons/png/${props.iconType}-24px/${props.icon.category}/${props.icon.id}.png`}
-                  download={`${props.icon.id}-24px.png`}
-                  className={styles.modalButton}
-                >
-                  <span>24px PNG</span>
-                </a>
-                <a
-                  href={`/icons/png/${props.iconType}-24px/${props.icon.category}/${props.icon.id}@2x.png`}
-                  download={`${props.icon.id}-24px@2x.png`}
+                  href={`/icons/png/${props.iconType}/${props.icon.category}/${props.icon.id}.png`}
+                  download={`${props.icon.id}.png`}
                   className={styles.modalButton}
                 >
                   <span>48px PNG</span>
                 </a>
+                <a
+                  href={`/icons/png/${props.iconType}/${props.icon.category}/${props.icon.id}@2x.png`}
+                  download={`${props.icon.id}@2x.png`}
+                  className={styles.modalButton}
+                >
+                  <span>96px PNG</span>
+                </a>
               </div>
-            </>
-          )}
+              {props.icon.formats.includes('24px') && (
+                <>
+                  <img
+                    src={`/icons/svg/${props.iconType}-24px/${props.icon.category}/${props.icon.id}.svg`}
+                    className={styles.modal24pxImage}
+                    width="24"
+                    height="24"
+                    alt=""
+                  />
+                  <div className={styles.modalLabel}>
+                    Small version (24px grid)
+                  </div>
+                  <div className={styles.modal24pxVersions}>
+                    <a
+                      href={`/icons/svg/${props.iconType}-24px/${props.icon.category}/${props.icon.id}.svg`}
+                      download={`${props.icon.id}-24px.svg`}
+                      className={styles.modalButton}
+                    >
+                      <span>SVG</span>
+                    </a>
+                    <a
+                      href={`/icons/png/${props.iconType}-24px/${props.icon.category}/${props.icon.id}.png`}
+                      download={`${props.icon.id}-24px.png`}
+                      className={styles.modalButton}
+                    >
+                      <span>24px PNG</span>
+                    </a>
+                    <a
+                      href={`/icons/png/${props.iconType}-24px/${props.icon.category}/${props.icon.id}@2x.png`}
+                      download={`${props.icon.id}-24px@2x.png`}
+                      className={styles.modalButton}
+                    >
+                      <span>48px PNG</span>
+                    </a>
+                  </div>
+                </>
+              )}
+            </div>
+            {iconCode ? (
+              <div className={styles.modalCopySvgContainer}>
+                <div className={styles.modalLabel}>Copy Svg Code</div>
+                <pre>{iconCode}</pre>
+                <button
+                  className={styles.modalButton}
+                  onClick={handleCopyToClipboard}
+                >
+                  <span>Copy SVG</span>
+                </button>
+              </div>
+            ) : null}
+          </div>
 
           {props.icon.tags.length > 0 && (
             <div className={styles.modalTagsSection}>
